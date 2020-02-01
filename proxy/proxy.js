76316e60f57c06aab3cdfb0436f70aed2bad9b67
Mark Wallace <mwallace999@gmail.com>
2020-02-01 13:08:33 -0600

    Editting errors fixed

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -9,32 +9,40 @@ const ServerTwo = 'http://localhost:8002';
 const ServerThree = 'http://localhost:8003';
 const ServerFour = 'http://localhost:8004';
 const ServerFive = 'http://localhost:8005';
+const ServerSix = 'http://localhost:8006';
 
 //Carousel server functions
-app.all('/getProducts/*', (req, res) => {
+//   localhost:8000/carousel/getProducts/0 (or categoryId)
+app.all('/carousel/*', (req, res) => {
   console.log('Redirecting to Carousel at localhost:8001');
   apiProxy.web(req, res, { target: Carousel });
 });
 
 app.all('/app2/*', (req, res) => {
-  console.log('redirecting to Server2');
+  console.log('redirecting to Server 2');
   apiProxy.web(req, res, { target: ServerTwo });
 });
 
 app.all('/app3/*', (req, res) => {
-  console.log('redirecting to Server3');
+  console.log('redirecting to Server 3');
   apiProxy.web(req, res, { target: ServerThree });
 });
 
 app.all('/app4/*', (req, res) => {
-  console.log('redirecting to Server3');
+  console.log('redirecting to Server 4');
   apiProxy.web(req, res, { target: ServerFour });
 });
 
 app.all('/app5/*', (req, res) => {
-  console.log('redirecting to Server3');
+  console.log('redirecting to Server 5');
   apiProxy.web(req, res, { target: ServerFive });
 });
+
+app.all('/app6/*', (req, res) => {
+  console.log('redirecting to Server 6');
+  apiProxy.web(req, res, { target: ServerSix });
+});
+
 
 app.listen(port, () => {
   console.log(`Proxy Listening on port: ${port}`);
